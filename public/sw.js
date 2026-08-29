2cdbc1c58a1174bf24161aa50882dc6ec8aaa18d
Cam <kitsboy@gmail.com>
2026-08-29 10:12:23 -0700

    chore: sync service worker cache name to v5.0.129

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tadbuy-v5.0.127';
+const CACHE_NAME = 'tadbuy-v5.0.129';
 const PRECACHE = ['/', '/favicon.png', '/favicon.svg', '/manifest.json', '/og-image.svg'];
 
 function expectedContentType(pathname) {
