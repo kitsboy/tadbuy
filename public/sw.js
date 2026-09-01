0c9231d08b92a566bce62db1e01a0bc4a0a91f38
Cam <kitsboy@gmail.com>
2026-09-01 16:17:01 -0700

    chore: sync sw.js cache to v5.0.139

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tadbuy-v5.0.137';
+const CACHE_NAME = 'tadbuy-v5.0.139';
 const PRECACHE = ['/', '/favicon.png', '/favicon.svg', '/manifest.json', '/og-image.svg'];
 
 function expectedContentType(pathname) {
