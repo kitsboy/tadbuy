fcdf804ddae7404c578f9e04dd14749f5610fd43
ziggy <ziggy@giveabit.io>
2026-09-13 20:21:12 +0200

    chore: auto-bump version to v5.0.171 (SW cache bust for the truth fixes)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tadbuy-v5.0.155';
+const CACHE_NAME = 'tadbuy-v5.0.171';
 const PRECACHE = ['/', '/favicon.png', '/favicon.svg', '/manifest.json', '/og-image.svg'];
 
 function expectedContentType(pathname) {
