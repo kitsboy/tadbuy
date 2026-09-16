b8021851a82a6371628b6b60bec988d9393ee857
Cam (via Aider) <cam@giveabit.io>
2026-09-16 22:50:49 +0200

    chore: stamp version v5.0.211 into sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tadbuy-v5.0.210';
+const CACHE_NAME = 'tadbuy-v5.0.211';
 const PRECACHE = ['/', '/favicon.png', '/favicon.svg', '/manifest.json', '/og-image.svg'];
 
 function expectedContentType(pathname) {
